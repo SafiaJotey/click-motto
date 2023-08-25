4bb4e30ad9c036df47b20a93c6d24ab155496eb3
SafiaJotey <safiaahmed1786@gmail.com>
2023-08-25 09:47:50 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'black',
+        secondary: '#006E61',
+        reviewColor: '#ffc108',
+        bannerBg: '#f2d6ef',
+        bgCircle: '#f2f2f2',
+        shape: 'rgba(224,247,251,.4)',
+        baseColor: 'rgba(129,131,139,.4)',
+      },
+    },
+  },
+  plugins: [],
+};
